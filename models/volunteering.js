2c03616e8ef0cafe5435cf814729fa73a8dd3610
markDeveloping <mark.developing@gmail.com>
2017-11-11 12:20:09 +0000

    add express validator.

--- models/volunteering.js
+++ models/volunteering.js
@@ -4,14 +4,14 @@ var Schema = mongoose.Schema;
 
 var VolunteeringSchema = Schema(
 		{
-			vol_title: {type: String, required: true, max: 100},
+			vol_title: {type: String, required: [true, "You need a title"], max: 100},
 			vol_location: {type: String, required: true, max: 100},
 			vol_description: {type: String, required: true},
 			vol_sponsor: {type: String, required: true},
 		}
 	);
 
-// Virtual for author's URL
+// Virtual for volunteer's URL
 VolunteeringSchema
 .virtual('url')
 .get(function () {
